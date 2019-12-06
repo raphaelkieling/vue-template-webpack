817fbadea1e3c2145f65bd596e68c017add2ef34
Raphael kieling <rapha_kieling@hotmail.com>
2019-12-05 22:18:15 -0300

    Fix terun

--- terun/terun.js
+++ terun/terun.js
@@ -8,19 +8,19 @@ module.exports = {
             transports: [
                 {
                     from: './templates/Index.terun',
-                    to: '../src/modules/{{entitiy | underscore | lowercase}}/Index.vue'
+                    to: '../src/modules/{{entity | underscore | lowercase}}/Index.vue'
                 },
                 {
                     from: './templates/index.terun',
-                    to: '../src/modules/{{entitiy | underscore | lowercase}}/index.ts'
+                    to: '../src/modules/{{entity | underscore | lowercase}}/index.ts'
                 },
                 {
                     from: './templates/List.terun',
-                    to: '../src/modules/{{entitiy | underscore | lowercase}}/List.vue'
+                    to: '../src/modules/{{entity | underscore | lowercase}}/List.vue'
                 },
                 {
                     from: './templates/router.terun',
-                    to: '../src/modules/{{entitiy | underscore | lowercase}}/router.ts'
+                    to: '../src/modules/{{entity | underscore | lowercase}}/router.ts'
                 }
             ]
         }
